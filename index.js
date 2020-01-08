c32d355c50f49952c59cf34dec23542bbddd55d6
Mike Gaudreau <gaudreaum01@gmail.com>
2020-01-08 10:51:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(str){
+    let newStr = str.split('')
+    newStr[0] = str[0].toLocaleUpperCase()
+    return newStr.join('')
+  }
+
+  static sanitize(str){
+    return str.replace(/[^A-Za-z0-9 '-]+/g, '')
+  }
+
+  static titleize(str){
+    const badWords = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    let strArr = str.split(' ')
+    for (const i in strArr){
+      if (i == 0 || !badWords.includes(strArr[i]) ){
+        strArr[i] = this.capitalize(strArr[i])
+      }
+    }
+    return strArr.join(' ')
+  }
+
 }
